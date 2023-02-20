9a60cc06d94ac7be8bec56358089ec4b32703e65
CodingManoj <mvp2612@hotmail.com>
2023-02-19 20:07:22 -0500

    Auto Push Commit

--- docdb.tf
+++ docdb.tf
@@ -5,7 +5,7 @@ resource "aws_docdb_cluster" "docdb" {
   engine                  = "docdb"
   master_username         = "foo"
   master_password         = "mustbeeightchars"
-#   backup_retention_period = 5
+#   backup_retention_period = 5                        Uncomment only when you need backups
 #   preferred_backup_window = "07:00-09:00"
 #   skip_final_snapshot     = true
 }
