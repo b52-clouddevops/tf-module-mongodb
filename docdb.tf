bd017f6b1418969a2d3eb9ed7df0293866f33813
CodingManoj <mvp2612@hotmail.com>
2023-02-19 20:08:23 -0500

    Auto Push Commit

--- docdb.tf
+++ docdb.tf
@@ -4,7 +4,7 @@ resource "aws_docdb_cluster" "docdb" {
   cluster_identifier      = "roboshop-${var.ENV}-docdb"
   engine                  = "docdb"
   master_username         = "admin1"
-  master_password         = "mustbeeightchars"
+  master_password         = "roboshop1"
 #   backup_retention_period = 5                        Uncomment only when you need backups
 #   preferred_backup_window = "07:00-09:00"
 #   skip_final_snapshot     = true
