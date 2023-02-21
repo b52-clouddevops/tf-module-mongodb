6b5eb30fc3b98ceb2caf6ff80aac4a1948fb8861
CodingManoj <mvp2612@hotmail.com>
2023-02-20 20:11:48 -0500

    Auto Push Commit

---
+++ security-group.tf
@@ -0,0 +1,33 @@
+# Creates Security Group.
+# Creates Security Group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH from Public"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # [] represent's list. 
+  }
+
+  ingress {
+    description = "HTTP from Public"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]   # [] represent's list. 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_sg"
+  }
+}
