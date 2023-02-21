ad5dc147aad3b7440ce4835e452ab72ad6a0e4ce
CodingManoj <mvp2612@hotmail.com>
2023-02-20 22:26:19 -0500

    AutoPush

--- docdb.tf
+++ docdb.tf
@@ -9,7 +9,7 @@ resource "aws_docdb_cluster" "docdb" {
   db_subnet_group_name    = aws_docdb_subnet_group.docdb.id
 #   backup_retention_period = 5                        Uncomment only when you need backups
 #   preferred_backup_window = "07:00-09:00"
-#   skip_final_snapshot     = true
+  skip_final_snapshot     = false
 }
 
 # Creates Subnet Group 
