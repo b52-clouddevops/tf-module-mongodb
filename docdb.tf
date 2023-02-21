06f5f0472a7ff1f41c456ba7fcceba5a17106c10
CodingManoj <mvp2612@hotmail.com>
2023-02-20 19:57:52 -0500

    Auto Push Commit

--- docdb.tf
+++ docdb.tf
@@ -22,3 +22,9 @@ resource "aws_docdb_subnet_group" "docdb" {
 }
 
 # Provision the nodes needed for doc-db and add them to the docdb cluster.
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  count              = 1
+  identifier         = "docdb-cluster-demo-${count.index}"
+  cluster_identifier = aws_docdb_cluster.default.id
+  instance_class     = "db.r5.large"
+}
