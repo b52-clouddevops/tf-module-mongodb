cffbe360d33f2f15ddeb0844b33ce76c4c035f41
CodingManoj <mvp2612@hotmail.com>
2023-02-20 20:02:55 -0500

    Auto Push Commit

--- docdb.tf
+++ docdb.tf
@@ -31,3 +31,5 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
 
 # Our application is not designed to work with Document DB. That's because of the fact that AWS don't let youto create Document DB without Username and password
 # Out cart and catalogue code is not designed to talk to mongodb with credentials.
+
+# Let's try to understand, how can we connect t
