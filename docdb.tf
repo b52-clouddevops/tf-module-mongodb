9a3e836dd07cf9dc017288860cc838207e5d6b67
CodingManoj <mvp2612@hotmail.com>
2023-02-20 19:58:31 -0500

    Auto Push Commit

--- docdb.tf
+++ docdb.tf
@@ -26,5 +26,5 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
   count              = 1
   identifier         = "docdb-cluster-demo-${count.index}"
   cluster_identifier = aws_docdb_cluster.default.id
-  instance_class     = "db.r5.large"
+  instance_class     = "db.t3.medium"
 }
