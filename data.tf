5a26e54aac0d3488ce4bce7acd119b15a05586e9
CodingManoj <mvp2612@hotmail.com>
2023-02-21 20:01:11 -0500

    Auto Push Commit

--- data.tf
+++ data.tf
@@ -1,7 +1,6 @@
 # Fetches the information of the remote statefile, here in our case, this will fetch the information of the VPC Statefile
 data "terraform_remote_state" "vpc" {
   backend = "s3"
-
   config = {
         bucket = "b52-terraform-state-bucket"
         key    = "vpc/${var.ENV}/terraform.tfstate"
