196c0e82da946480fdd16e2835a51e76f2d8ccd5
CodingManoj <mvp2612@hotmail.com>
2023-02-22 20:33:38 -0500

    Auto Push Commit

--- schema.tf
+++ schema.tf
@@ -6,10 +6,11 @@ resource "null_resource" "docdb-schema" {
   provisioner "local-exec" {
         command = <<EOF
         cd /tmp 
+        wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
         curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
         unzip -o mongodb.zip
         cd mongodb-main
-        wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
+
 mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js 
 mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < users.js 
         EOF
