e7516bfe1441a7dfe784b6ba5736ab21b8c54d44
CodingManoj <mvp2612@hotmail.com>
2023-02-22 20:35:10 -0500

    Auto Push Commit

--- schema.tf
+++ schema.tf
@@ -10,7 +10,6 @@ resource "null_resource" "docdb-schema" {
         curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
         unzip -o mongodb.zip
         cd mongodb-main
-
         mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js 
         mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < users.js 
         EOF
