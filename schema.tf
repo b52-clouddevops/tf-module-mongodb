5f6ea3b3d828afb2f718fbe880ed9a7d03bc4539
CodingManoj <mvp2612@hotmail.com>
2023-02-22 20:22:49 -0500

    Auto Push Commit

---
+++ schema.tf
@@ -0,0 +1,15 @@
+resource "null_resource" "docdb-schema" {
+  
+  # This is how we can create depenency and ensure this will only run after the creation if the RDS Instance.
+  depends_on = [aws_db_instance.mysql5]
+
+  provisioner "local-exec" {
+        command = <<EOF
+        cd /tmp 
+        curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+        unzip mysql.zip 
+        cd mysql-main 
+        mysql -h ${aws_db_instance.mysql5.address} -u admin1 -pRoboShop1 <shipping.sql
+        EOF
+  }  
+}
