03b141e925fd9e6ad0888e907f9b61761e9f2a94
CodingManoj <mvp2612@hotmail.com>
2023-02-22 20:31:13 -0500

    Auto Push Commit

--- schema.tf
+++ schema.tf
@@ -10,7 +10,7 @@ resource "null_resource" "docdb-schema" {
         unzip -o mongodb.zip
         cd mongodb-main
         wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
-        mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1
+mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile /tmp/rds-combined-ca-bundle.pem --username admin1 --password roboshop1 < catalogue.js 
         mongo < catalogue.js
         EOF
     }  
