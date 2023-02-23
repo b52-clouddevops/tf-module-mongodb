051c830e0256e1f332617db804b5b1e848b3aef5
CodingManoj <mvp2612@hotmail.com>
2023-02-22 20:23:09 -0500

    Auto Push Commit

--- schema.tf
+++ schema.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "docdb-schema" {
   
   # This is how we can create depenency and ensure this will only run after the creation if the RDS Instance.
-  depends_on = [aws_db_instance.mysql5]
+  depends_on = ["aws_docdb_cluster.docdb]
 
   provisioner "local-exec" {
         command = <<EOF
