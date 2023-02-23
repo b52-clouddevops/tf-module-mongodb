e6470d8f91f43f31d9eb6606968dc77eb2aba181
CodingManoj <mvp2612@hotmail.com>
2023-02-22 20:23:35 -0500

    Auto Push Commit

--- schema.tf
+++ schema.tf
@@ -6,10 +6,7 @@ resource "null_resource" "docdb-schema" {
   provisioner "local-exec" {
         command = <<EOF
         cd /tmp 
-        curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-        unzip mysql.zip 
-        cd mysql-main 
-        mysql -h ${aws_db_instance.mysql5.address} -u admin1 -pRoboShop1 <shipping.sql
+
         EOF
   }  
 }
