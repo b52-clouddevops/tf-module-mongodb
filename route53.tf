9153a0c461c6fff6f0c1eeef5c8f0931e9c59c3f
CodingManoj <mvp2612@hotmail.com>
2023-02-23 20:07:39 -0500

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -2,7 +2,7 @@
 # Creates CNAME record for the docdb endpoint.
 resource "aws_route53_record" "record" {
   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID
-  name    = "mongodb-${var.ENV}.${}"
+  name    = "mongodb-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_NAME}"
   type    = "CNAME"
   ttl     = 300
   records = [aws_eip.lb.public_ip]
