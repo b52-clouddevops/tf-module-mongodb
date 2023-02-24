bc6ced5f4287b4af9139d203ff72dd03bf005ae7
CodingManoj <mvp2612@hotmail.com>
2023-02-23 20:07:08 -0500

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -3,7 +3,7 @@
 resource "aws_route53_record" "record" {
   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID
   name    = "www.example.com"
-  type    = "A"
+  type    = "CNAME"
   ttl     = 300
   records = [aws_eip.lb.public_ip]
 }
