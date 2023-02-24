1f9f0b2d585a393e09971cde0a089322cfdc5e6e
CodingManoj <mvp2612@hotmail.com>
2023-02-23 19:49:58 -0500

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -1,11 +1,11 @@
 # Creates CNAME record for the docdb endpoint.
-resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "www.example.com"
-  type    = "A"
-  ttl     = 300
-  records = [aws_eip.lb.public_ip]
-}
+# resource "aws_route53_record" "www" {
+#   zone_id = aws_route53_zone.primary.zone_id
+#   name    = "www.example.com"
+#   type    = "A"
+#   ttl     = 300
+#   records = [aws_eip.lb.public_ip]
+# }
 
 # The scope of a Route53 Zone is by default works in the default-vpc
 # If you want DNS Resoutions should work in a specific VPC-x, you need to ensure that your Route53-zone is associated to VPC-x
