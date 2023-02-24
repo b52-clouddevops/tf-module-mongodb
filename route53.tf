53fedd1b0205a575f41725b51c9e7574ddbfac32
CodingManoj <mvp2612@hotmail.com>
2023-02-23 19:43:21 -0500

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -8,4 +8,4 @@ resource "aws_route53_record" "www" {
 }
 
 # The scope of a Route53 Zone is by default works in the default-vpc
-# If you want DNS Resoutions should work in a specific VPC-x
+# If you want DNS Resoutions should work in a specific VPC-x, you need to ensure that your Route53-zone is associated to VPC-x
