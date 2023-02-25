732e5a752504543f6d05f593fd1cc8322ee19d4a
CodingManoj <mvp2612@hotmail.com>
2023-02-24 19:57:32 -0500

    Auto Push Commit

--- locals.tf
+++ locals.tf
@@ -1,1 +1,5 @@
-# Whenever you have some common fileds or 
+# Whenever you have some common fields or anything which is highly used, you can consider that in locals and can call it on your need
+
+locals {
+    DOCDB_USER 
+}
