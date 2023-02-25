aaf0a86a842ef83954428fd8ae014db6b281a36c
CodingManoj <mvp2612@hotmail.com>
2023-02-24 19:49:32 -0500

    Auto Push Commit

--- data.tf
+++ data.tf
@@ -16,3 +16,7 @@ data "aws_secretsmanager_secret" "secrets" {
 data "aws_secretsmanager_secret_version" "secrets" {
   secret_id = data.aws_secretsmanager_secret.secrets.id
 }
+
+output "data" {
+  value  =  data.
+}
