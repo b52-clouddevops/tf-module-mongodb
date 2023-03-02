a0d810f8a8139e37ec0ca4ad23d6e1d6755bb86e
CodingManoj <mvp2612@hotmail.com>
2023-03-01 20:47:17 -0500

    Auto Push Commit

--- locals.tf
+++ locals.tf
@@ -3,4 +3,5 @@
 locals {
     DOCDB_USER = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_USERNAME"]
     DOCDB_PASS = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_PASSWORD"]
+    DNS_NAME   = aws_docdb_cluster.docdb.endpoint
 }
